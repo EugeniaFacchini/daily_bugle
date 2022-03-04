058083bb668fa1f3999846547850f5dc8ba02d36
thomas <thomas.giunchedi@gmail.com>
2022-03-04 16:45:47 +0100

    commit errori

--- lib/screens/favourites_screen.dart
+++ lib/screens/favourites_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:daily_bugle/models/news_model.dart';
-import 'package:daily_bugle/widgets/news.dart';
+import 'package:daily_bugle/widgets/news_list.dart';
 
 class FavouriteScreen extends StatefulWidget {
   final List<News> elements;
